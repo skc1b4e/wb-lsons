f7eb68cb8ec44ff7f1fa8af6b94f6559db736418
ajithkumar23k <85283339+ajithkumar23k@users.noreply.github.com>
2021-07-25 23:27:08 +0530

    Add files via upload

---
+++ pagination remote json dropdown without jquery/script.js
@@ -0,0 +1,136 @@
+ 
+
+  
+ function jsFunction(value)
+{
+	var myArray = [];
+    // var e = document.getElementById("limitNumberOfResultsPerPage");
+    //  console.log(e.value)
+
+	 
+		var xhr = new XMLHttpRequest();
+		
+    	xhr.open('GET','https://raw.githubusercontent.com/dr5hn/countries-states-cities-database/master/cities.json',true);
+    
+        
+		xhr.onload = function()
+		{
+			var myArray = JSON.parse(xhr.responseText);
+            
+			console.log(myArray);
+            var e = document.getElementById("limitNumberOfResultsPerPage");
+            var value = e.options[e.selectedIndex].value;
+        
+            /*
+            1 - Loop Through Array & Access each value
+            2 - Create Table Rows & append to table
+            */
+		 
+            var state = {
+            'querySet': myArray,
+            
+            'page': 1,
+            'rows': value,
+            'window': 5,
+            }
+			var table = document.getElementById("our-table");
+						
+			var rowCount = table.rows.length;
+			for (var i=rowCount-1; i >=0; i--) {
+				table.deleteRow(i);
+			}
+          
+            buildTable()
+            
+            function pagination(querySet, page, rows) {
+            
+            var trimStart = (page - 1) * rows
+            var trimEnd = trimStart + rows
+            
+            var trimmedData = querySet.slice(trimStart, trimEnd)
+            
+            var pages = Math.round(querySet.length / rows);
+            
+            return {
+                'querySet': trimmedData,
+                'pages': pages,
+            }
+            }
+            
+            function pageButtons(pages) {
+            var wrapper = document.getElementById('pagination-wrapper')
+            
+            wrapper.innerHTML = ``
+            console.log('Pages:', pages)
+            
+            var maxLeft = (state.page - Math.floor(state.window / 2))
+            var maxRight = (state.page + Math.floor(state.window / 2))
+            
+            if (maxLeft < 1) {
+                maxLeft = 1
+                maxRight = state.window
+            }
+            
+            if (maxRight > pages) {
+                maxLeft = pages - (state.window - 1)
+                
+                if (maxLeft < 1){
+                    maxLeft = 1
+                }
+                maxRight = pages
+            }
+            
+            
+            
+            for (var page = maxLeft; page <= maxRight; page++) {
+                wrapper.innerHTML += `<button value=${page} class="page btn btn-sm btn-info">${page}</button>`
+            }
+            
+            if (state.page != 1) {
+                wrapper.innerHTML = `<button value=${1} class="page btn btn-sm btn-info">&#171; First</button>` + wrapper.innerHTML
+            }
+            
+            if (state.page != pages) {
+                wrapper.innerHTML += `<button value=${pages} class="page btn btn-sm btn-info">Last &#187;</button>`
+            }
+            
+            document.querySelector('.page').addEventListener('click', function() {
+                document.querySelector('#table-body').empty()
+            
+                state.page = Number(document.querySelector(this).value)
+            
+                buildTable()
+            })
+            
+            }
+            
+            
+            function buildTable() {
+            var table = document.querySelector('#table-body')
+            
+            var data = pagination(state.querySet, state.page, state.rows)
+            var myList = data.querySet
+            
+            for (var i = 1 in myList) {
+                //Keep in mind we are using "Template Litterals to create rows"
+                var row = `<tr>
+                          <td>${myList[i].id}</td>
+                          <td>${myList[i].name}</td>
+                          <td>${myList[i].state_code}</td>
+                          <td>${myList[i].country_code}</td>
+						  </tr>
+                          `
+                table.insertAdjacentHTML("beforeend",row)
+            }
+            
+            pageButtons(data.pages)
+            }
+            
+				
+		}
+        xhr.setRequestHeader('Accept', 'application/json');
+		xhr.send();
+	}
+	jsFunction(value=10);
+
+ 
